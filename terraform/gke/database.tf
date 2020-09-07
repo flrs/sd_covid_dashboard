95b13a53512bf2ca3272b2c11ecb54c5a15c1420
flrs <9593883+flrs@users.noreply.github.com>
2020-09-07 08:59:54 -0700

    Fix private IP address setup

--- terraform/gke/database.tf
+++ terraform/gke/database.tf
@@ -1,6 +1,6 @@
 resource "google_compute_global_address" "private_ip_address" {
   provider = google-beta
-
+  project = var.project
   name          = "private-ip-address"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
@@ -10,7 +10,6 @@ resource "google_compute_global_address" "private_ip_address" {
 
 resource "google_service_networking_connection" "private_vpc_connection" {
   provider = google-beta
-
   network                 = google_compute_network.gke-network.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
