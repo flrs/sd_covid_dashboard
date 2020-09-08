ca44cb31dd13bd9b17ce4a8d2822cf4ff9991571
flrs <9593883+flrs@users.noreply.github.com>
2020-09-07 17:41:54 -0700

    Update config

--- terraform/gke/database.tf
+++ terraform/gke/database.tf
@@ -5,12 +5,12 @@ resource "google_compute_global_address" "private_ip_address" {
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
-  network       = google_compute_network.gke-network.id
+  network       = google_compute_network.gke-network.self_link
 }
 
 resource "google_service_networking_connection" "private_vpc_connection" {
   provider = google-beta
-  network                 = google_compute_network.gke-network.id
+  network                 = google_compute_network.gke-network.self_link
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
